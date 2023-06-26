5087ceb3734f04cf1352d7bc20dc6d8d52f8e389
wanton raven <josephngalu96@gmail.com>
2023-06-26 19:53:04 +0000

    changed all to let

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
     let task = true;
