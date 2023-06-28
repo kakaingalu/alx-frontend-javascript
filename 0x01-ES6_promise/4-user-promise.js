9d5e4ccd5691220bfc2454a4a1223dbef704f4de
wanton raven <josephngalu96@gmail.com>
2023-06-28 16:25:56 +0000

    Simple promise

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,7 @@
+function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    resolve({ firstName: `${firstName}`, lastName: `${lastName}` });
+  });
+}
+
+export default signUpUser;
