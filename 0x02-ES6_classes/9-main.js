569bb4e07b2f0f3db1f5ac8843fbda58d8d8d6e2
wantonraven <josephngalu96@gmail.com>
2023-06-29 23:40:21 +0300

    solved errors

--- 0x02-ES6_classes/9-main.js
+++ 0x02-ES6_classes/9-main.js
@@ -1,9 +1,7 @@
-import { listOfStudents } from './9-hoisting';
+import listOfStudents from "./9-hoisting.js";
 
 console.log(listOfStudents);
 
 const listPrinted = listOfStudents.map(
-  (student) => student.fullStudentDescription,
+    student => student.fullStudentDescription
 );
-
-console.log(listPrinted);
